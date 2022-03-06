eb857223f187574cfdb663c9da923ee49a0428e0
Melvin. L. Abraham <melvinredvirus@gmail.com>
2022-03-06 21:32:22 +0530

    feat: Include `TodoList` in `App` with callbacks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FilterOption, TodoItem } from './types';
 import AddTodoBox from './components/AddTodoBox/AddTodoBox';
+import TodoList from './components/TodoList/TodoList';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState<TodoItem[]>([]);
+  const [filter, setFilter] = useState<FilterOption>(FilterOption.all);
+
+  const editTodo = (
+    id: string,
+    updatedTodoCallback: (todoItem: TodoItem) => Omit<TodoItem, "id">
+  ) => {
+    const updatedTodoList = todoList.map((todoItem) => {
+      if (todoItem.id === id) {
+        return {
+          ...todoItem,
+          ...updatedTodoCallback(todoItem),
+        };
+      }
+
+      return todoItem;
+    });
+
+    setTodoList(updatedTodoList);
+  }
+
+  const onDelete = (id: string) => {
+    const updatedTodoList = todoList.filter((todoItem) => todoItem.id !== id);
+    setTodoList(updatedTodoList);
+  }
+
+  const onTaskNameEdit = (id: string, newName: string) => {
+    editTodo(id, (todoItem) => ({
+      task: newName,
+      completed: todoItem.completed,
+    }));
+  }
+
+  const onToggleCompletion = (id: string) => {
+    editTodo(id, (todoItem) => ({
+      task: todoItem.task,
+      completed: !todoItem.completed,
+    }));
+  }
+
   return (
     <div className="app-root">
       <AddTodoBox onAddTodo={() => {}} />
+      <TodoList
+        todoList={todoList}
+        filter={filter}
+        onDelete={onDelete}
+        onEdit={onTaskNameEdit}
+        onToggleCompletion={onToggleCompletion}
+      />
     </div>
   )
 }
