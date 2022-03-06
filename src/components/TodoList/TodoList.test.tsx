00a01f96a0514b3937671fb05ff75e3fef62c1e9
Melvin. L. Abraham <melvinredvirus@gmail.com>
2022-03-06 14:24:49 +0530

    test: Add interaction tests for `TodoList`

--- src/components/TodoList/TodoList.test.tsx
+++ src/components/TodoList/TodoList.test.tsx
@@ -11,6 +11,9 @@ const mockTodoItems: TodoItem[] = [
   { id: '4', task: 'Task 4', completed: false },
 ];
 
+// Mock `window.prompt`
+window.prompt = jest.fn();
+
 let onEdit: jest.Mock<any, any>;
 let onDelete: jest.Mock<any, any>;
 let onToggleCompletion: jest.Mock<any, any>;
@@ -31,49 +34,91 @@ function renderTodoList(todoItems: TodoItem[], filterOption: FilterOption = Filt
   );
 }
 
-it('should display an empty state when no todos are present', async () => {
-  renderTodoList([]);
+describe('Basic component rendering', () => {
+  it('should display an empty state when no todos are present', async () => {
+    renderTodoList([]);
 
-  const emptyStateElement = await screen.findByTestId('todo-list-empty');
-  expect(emptyStateElement).toBeInTheDocument();
+    const emptyStateElement = await screen.findByTestId('todo-list-empty');
+    expect(emptyStateElement).toBeInTheDocument();
+  });
+
+  it('should be able to properly display provided todos', async () => {
+    renderTodoList(mockTodoItems);
+    const taskNames = mockTodoItems.map((todoItem) => todoItem.task);
+
+    for (const taskName of taskNames) {
+      const taskTextElement = await screen.findByText(taskName);
+      expect(taskTextElement.textContent).toEqual(taskName);
+    }
+  });
+
+  it('should be able to properly filter pending tasks', async () => {
+    renderTodoList(mockTodoItems, FilterOption.pending);
+
+    const pendingTasks = mockTodoItems.filter((todoItem) => !todoItem.completed);
+    const taskNames = pendingTasks.map((todoItem) => todoItem.task);
+
+    const listItems = screen.getAllByRole('listitem');
+    expect(pendingTasks.length).toEqual(listItems.length);
+
+    for (const taskName of taskNames) {
+      const taskTextElement = await screen.findByText(taskName);
+      expect(taskTextElement.textContent).toEqual(taskName);
+    }
+  });
+
+  it('should be able to properly filter completed tasks', async () => {
+    renderTodoList(mockTodoItems, FilterOption.completed);
+
+    const completedTasks = mockTodoItems.filter((todoItem) => todoItem.completed);
+    const taskNames = completedTasks.map((todoItem) => todoItem.task);
+
+    const listItems = screen.getAllByRole('listitem');
+    expect(completedTasks.length).toEqual(listItems.length);
+
+    for (const taskName of taskNames) {
+      const taskTextElement = await screen.findByText(taskName);
+      expect(taskTextElement.textContent).toEqual(taskName);
+    }
+  });
 });
 
-it('should be able to properly display provided todos', async () => {
-  renderTodoList(mockTodoItems);
-  const taskNames = mockTodoItems.map((todoItem) => todoItem.task);
+describe('User interaction testing', () => {
+  it('should call delete callback with proper params when delete is pressed', async () => {
+    const minimalTodoList = mockTodoItems.slice(0, 1);
+    renderTodoList(minimalTodoList);
 
-  for (const taskName of taskNames) {
-    const taskTextElement = await screen.findByText(taskName);
-    expect(taskTextElement.textContent).toEqual(taskName);
-  }
-});
+    const deleteTodoButton = await screen.findByTestId('todo-list-item-delete-button');
+    userEvent.click(deleteTodoButton);
 
-it('should be able to properly filter pending tasks', async () => {
-  renderTodoList(mockTodoItems, FilterOption.pending);
+    expect(onDelete).toHaveBeenCalledTimes(1);
+    expect(onDelete).toHaveBeenCalledWith(mockTodoItems[0].id);
+  });
 
-  const pendingTasks = mockTodoItems.filter((todoItem) => !todoItem.completed);
-  const taskNames = pendingTasks.map((todoItem) => todoItem.task);
+  it('should call edit callback when edit is pressed', async () => {
+    // Prevent being mocked for other tests
+    (window.prompt as any).mockClear();
 
-  const listItems = screen.getAllByRole('listitem');
-  expect(pendingTasks.length).toEqual(listItems.length);
+    const minimalTodoList = mockTodoItems.slice(0, 1);
+    renderTodoList(minimalTodoList);
 
-  for (const taskName of taskNames) {
-    const taskTextElement = await screen.findByText(taskName);
-    expect(taskTextElement.textContent).toEqual(taskName);
-  }
-});
+    const editTodoButton = await screen.findByTestId('todo-list-item-edit-button');
+    userEvent.click(editTodoButton);
 
-it('should be able to properly filter completed tasks', async () => {
-  renderTodoList(mockTodoItems, FilterOption.completed);
+    expect(onEdit).toHaveBeenCalledTimes(1);
 
-  const completedTasks = mockTodoItems.filter((todoItem) => todoItem.completed);
-  const taskNames = completedTasks.map((todoItem) => todoItem.task);
+    // @warn: Coupled with impl. details
+    expect(window.prompt).toHaveBeenCalled();
+  });
 
-  const listItems = screen.getAllByRole('listitem');
-  expect(completedTasks.length).toEqual(listItems.length);
+  it('should call toggle task completion callback with proper params when checkbox is toggled', async () => {
+    const minimalTodoList = mockTodoItems.slice(0, 1);
+    renderTodoList(minimalTodoList);
 
-  for (const taskName of taskNames) {
-    const taskTextElement = await screen.findByText(taskName);
-    expect(taskTextElement.textContent).toEqual(taskName);
-  }
+    const taskCheckbox = await screen.findByRole('checkbox');
+    userEvent.click(taskCheckbox);
+
+    expect(onToggleCompletion).toHaveBeenCalledTimes(1);
+    expect(onToggleCompletion).toHaveBeenCalledWith(mockTodoItems[0].id);
+  });
 });
